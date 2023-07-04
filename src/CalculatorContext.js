2b59a81513d912d8a7d5d3e81812bc3a95f434cf
GreatDeveloper66 <ashaffe@gmail.com>
2023-07-04 18:38:48 -0500

    Fix calculator numeral bug

--- src/CalculatorContext.js
+++ src/CalculatorContext.js
@@ -8,54 +8,95 @@ const CalculatorProvider = ({ children }) => {
         expression: ''
     };
 
+    const isOperator = value => {
+        return value === '+' || value === '-' || value === '*' || value === '/';
+        };
+
 
     const [state, setState] = useState(initialState);
 
     const handleButtonClick = value => {
         setState(prevState => {
-            let newDisplayValue = prevState.displayValue;
-            let newExpression = prevState.expression;
-
-            switch (value) {
-                case 'C':
-                    // Clear the display and expression
-                    newDisplayValue = '0';
-                    newExpression = '';
-                    break;
-                case '=':
-                    // Perform calculations
-                    try {
-                        const result = eval(newExpression);
-                        newExpression = result.toString();
-                        newDisplayValue = result.toString(); // Set the current number as display value
-                    } catch (error) {
-                        // Handle calculation errors
-                        newDisplayValue = 'Error';
-                        newExpression = '';
-                    }
-                    // ...logic for '=' case as shown above...
-                    break;
-                case 'DEL':
-                    // Delete the last character from the display and expression
-                    newDisplayValue = newDisplayValue.slice(0, -1);
-                    newExpression = newExpression.slice(0, -1);
-                    break;
-                default:
-                    // Update the display and expression with the button value
-                    newDisplayValue = prevState.displayValue === '0' ? value : newDisplayValue + value;
-                    newExpression += value;
-                    break;
-            }
-
-            return {
-                ...prevState,
-                displayValue: newDisplayValue,
-                expression: newExpression
-            };
+          let newDisplayValue = prevState.displayValue;
+          let newExpression = prevState.expression;
+      
+          switch (value) {
+            case 'C':
+              // Clear the display and expression
+              newDisplayValue = '';
+              newExpression = '';
+              break;
+            case '=':
+              // Perform calculations
+              try {
+                const result = eval(newExpression);
+                newDisplayValue = result.toString();
+                newExpression = result.toString();
+              } catch (error) {
+                newDisplayValue = 'Error';
+                newExpression = '';
+              }
+              break;
+            case 'DEL':
+              // Delete the last character from the display and expression
+              newDisplayValue = newDisplayValue.slice(0, -1);
+              newExpression = newExpression.slice(0, -1);
+              break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+              // Handle operators
+              if (endsWithOperator(newExpression)) {
+                // Replace the last operator with the new one
+                newExpression = newExpression.slice(0, -1) + value;
+              } else {
+                newExpression += value;
+              }
+              break;
+            default:
+              // Handle numbers
+              if (newDisplayValue === '0' || isOperator(newDisplayValue) || newDisplayValue === 'Error' || endsWithOperator(newExpression)) {
+                newDisplayValue = value;
+              } else {
+                newDisplayValue += value;
+              }
+              newExpression += value;
+              break;
+          }
+      
+          return {
+            ...prevState,
+            displayValue: newDisplayValue,
+            expression: newExpression
+          };
         });
-    };
-
-
+      };
+      
+      const endsWithOperator = expression => {  
+        const operators = ['+', '-', '*', '/'];
+        return operators.some(operator => expression.endsWith(operator));
+        };
+    
+      
+      
+      
+      const getOperand = expression => {
+        const operators = ['+', '-', '*', '/'];
+        const expressionParts = expression.split(' ');
+      
+        for (let i = expressionParts.length - 1; i >= 0; i--) {
+          const part = expressionParts[i];
+      
+          if (part && !operators.includes(part)) {
+            return part;
+          }
+        }
+      
+        return '';
+      };
+      
+          
     const contextValue = {
         state,
         handleButtonClick
