87e54b83d29c8053ab8fb4fc2548e7a5bd06e441
GreatDeveloper66 <ashaffe@gmail.com>
2023-10-21 17:54:12 -0500

    create handleEquals

---
+++ src/utilities/handleEquals.js
@@ -0,0 +1,26 @@
+export const handleEquals = (prevState) => {
+    let { displayValue, expression } = prevState;
+    try {
+        if (expression.endsWith('Math.sqrt(')) {
+          // If the expression ends with 'Math.sqrt(', it means the square root operation is not complete
+          displayValue = 'Error';
+          expression = '';
+        } else {
+          // Append a closing parenthesis before evaluating to complete any pending square root operation
+          if (expression.includes('Math.sqrt(')) {
+            expression += ')';
+          }
+          let result = Function('return ' + expression)().toString();
+          displayValue = result;
+          expression = result;
+        }
+      } catch (error) {
+        displayValue = 'Error';
+        expression = '';
+      }
+    return {
+        ...prevState,
+        displayValue: displayValue,
+        expression: expression
+    };
+}
